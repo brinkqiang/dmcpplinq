3e92e8c041363289f87dc5b2ea193135df178fc0
brinkqiang <brink.qiang@gmail.com>
2022-05-11 15:02:31 +0800

    add

--- test/TakeWhileTest/TakeWhileTest.cpp
+++ test/TakeWhileTest/TakeWhileTest.cpp
@@ -6,10 +6,10 @@
 TEST(TakeWhile, ManyToMore)
 {
     int src[] = { 1, 2, 3, 4, 5, 6 };
-    int ans[] = { 1, 2, 3, 4, 5, 6 };
+    int ans[] = { 1, 2 , 3, 4, 5 };
 
     auto rng = cpplinq::From(src);
-    auto dst = rng.TakeWhile([](int it) {return it >= 0 && it <= 10; });
+    auto dst = rng.TakeWhile([](int it) {return it >= 1 && it <= 5; });
 
     IsEqualArray(dst, ans);
 }
